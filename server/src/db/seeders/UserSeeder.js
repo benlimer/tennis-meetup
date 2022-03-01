9df1c422e2dcd8df73155c9999e35eff6814b226
benlimer <thlim940913@gmail.com>
2022-03-01 01:38:56 -0500

    additional styling and seeded more users

--- server/src/db/seeders/UserSeeder.js
+++ server/src/db/seeders/UserSeeder.js
@@ -33,7 +33,36 @@ class UserSeeder {
       age:35,
       image: "https://tennis-meetup-development.s3.amazonaws.com/nadal.jpeg"
     });
-    
+    await User.query().insert({
+      name: "Emma",
+      email: "e@e.com",
+      password: "password",
+      skillLevel: skillLevelData.skillLevel[4],
+      location: "216 Norfolk St, Cambridge, MA 02139",
+      gender: "F",
+      age:21,
+      image: "https://tennis-meetup-development.s3.amazonaws.com/Emma.jpeg"
+    });
+    await User.query().insert({
+      name: "Sharapova",
+      email: "s@s.com",
+      password: "password",
+      skillLevel: skillLevelData.skillLevel[5],
+      location: "10 ST CHARLES ST, BOSTON, MA, 02116",
+      gender: "F",
+      age:39,
+      image: "https://tennis-meetup-development.s3.amazonaws.com/sharapova.jpeg"
+    });
+    await User.query().insert({
+      name: "Coco",
+      email: "c@c.com",
+      password: "password",
+      skillLevel: skillLevelData.skillLevel[4],
+      location: "2 HOLBROOK AVE, BOSTON, MA, 02122",
+      gender: "F",
+      age:22,
+      image: "https://tennis-meetup-development.s3.amazonaws.com/coco.jpeg"
+    });
   }
 }
 
