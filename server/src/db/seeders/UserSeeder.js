6eb1faf24bb2e51d300358de4be7eca4ab5a4731
benlimer <thlim940913@gmail.com>
2022-03-10 16:56:36 -0500

    refactored geocoder and getDistance function

--- server/src/db/seeders/UserSeeder.js
+++ server/src/db/seeders/UserSeeder.js
@@ -8,7 +8,7 @@ class UserSeeder {
       email: "d@d.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[5],
-      location: "69 adams st, Somerville, MA 02145",
+      location: "02145",
       gender: "M",
       age: 34,
       image: "https://tennis-meetup-development.s3.amazonaws.com/djokovic.jpeg"
@@ -18,7 +18,7 @@ class UserSeeder {
       email: "f@f.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[5],
-      location: "12209 Fairfield House Dr, Fairfax, VA 22033",
+      location: "22033",
       gender: "M",
       age: 40,
       image: "https://tennis-meetup-development.s3.amazonaws.com/federer.jpeg"
@@ -28,7 +28,7 @@ class UserSeeder {
       email: "n@n.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[5],
-      location: "132 Dover St, Medford, MA 02155",
+      location: "02155",
       gender: "M",
       age:35,
       image: "https://tennis-meetup-development.s3.amazonaws.com/nadal.jpeg"
@@ -38,7 +38,7 @@ class UserSeeder {
       email: "e@e.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[4],
-      location: "216 Norfolk St, Cambridge, MA 02139",
+      location: "02139",
       gender: "F",
       age:21,
       image: "https://tennis-meetup-development.s3.amazonaws.com/Emma.jpeg"
@@ -48,7 +48,7 @@ class UserSeeder {
       email: "s@s.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[5],
-      location: "10 ST CHARLES ST, BOSTON, MA, 02116",
+      location: "02116",
       gender: "F",
       age:39,
       image: "https://tennis-meetup-development.s3.amazonaws.com/sharapova.jpeg"
@@ -58,7 +58,7 @@ class UserSeeder {
       email: "c@c.com",
       password: "password",
       skillLevel: skillLevelData.skillLevel[4],
-      location: "2 HOLBROOK AVE, BOSTON, MA, 02122",
+      location: "02122",
       gender: "F",
       age:22,
       image: "https://tennis-meetup-development.s3.amazonaws.com/coco.jpeg"
--- server/src/services/getDistance.js
+++ server/src/services/getDistance.js
@@ -1,16 +1,16 @@
 function getDistance(lat1, lon1, lat2, lon2) 
 {
-  var R = 6371; // km
-  var dLat = toRad(lat2-lat1);
-  var dLon = toRad(lon2-lon1);
-  var lat1 = toRad(lat1);
-  var lat2 = toRad(lat2);
+  let RadiusOfEarthInKm = 6371; // km
+  let radianLat = toRad(lat2-lat1);
+  let radianLon = toRad(lon2-lon1);
+  let radianLat1 = toRad(lat1);
+  let radianLat2 = toRad(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c;
-  return d;
+  let a = Math.sin(radianLat/2) * Math.sin(radianLat/2) +
+    Math.sin(radianLon/2) * Math.sin(radianLon/2) * Math.cos(radianLat1) * Math.cos(radianLat2); 
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  let distanceInKm = RadiusOfEarthInKm * c;
+  return distanceInKm;
 }
 
 // Converts numeric degrees to radians
